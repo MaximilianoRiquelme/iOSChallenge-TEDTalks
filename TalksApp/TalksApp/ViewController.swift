6eef6a5e91a977cfedf4db9fac23b587e9d0c075
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2021-08-03 10:35:52 -0300

    Project created

---
+++ TalksApp/TalksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalksApp
+//
+//  Created by Maximiliano Riquelme Vera on 03/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
